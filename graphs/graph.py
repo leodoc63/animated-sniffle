b883f08932cd533dd509770436dc1f90eedce346
oldoc <oldoc@localhost.localdomain>
2020-10-16 17:59:19 -0400

    Finding a Path I

--- graphs/graph.py
+++ graphs/graph.py
@@ -12,4 +12,12 @@ class Graph:
     self.graph_dict[from_vertex.value].add_edge(to_vertex.value, weight)
     if not self.directed:
       self.graph_dict[to_vertex.value].add_edge(from_vertex.value, weight)
+
+  def find_path(self, start_vertex, end_vertex):
+    print("Searchin from {0} to {1}".format(start_vertex, end_vertex))
+    start = [start_vertex]
+    while start:
+      current_vertex = start.pop(0)
+      print("Current Vertex: " + current_vertex)
+
     
