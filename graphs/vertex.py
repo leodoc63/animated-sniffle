c671f4a07fe09f035ffb7950acd8c522fcfde87b
oldoc <oldoc@localhost.localdomain>
2020-10-16 11:46:27 -0400

    Adding Weight

--- graphs/vertex.py
+++ graphs/vertex.py
@@ -1,10 +1,11 @@
 class Vertex:
-  def __init__(self, value):
-    self.value = value
-    self.edges = {}
+    def __init__(self, value):
+        self.value = value
+        self.edges ={}
 
-  def add_edge(self, vertex):
-    self.edges[vertex] = True
+    def add_edge(self, vertex):
+        self.edges[vertex] = True
 
-  def get_edges(self):
-    return list(self.edges.keys())
+    def get_edges(self):
+        return list(self.edges.keys())
+
