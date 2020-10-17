c1933b1829a656ad6c86aa30a2e32f169fb9b049
oldoc <oldoc@localhost.localdomain>
2020-10-17 12:41:27 -0400

    Account for cycles

--- graphs/graph.py
+++ graphs/graph.py
@@ -5,25 +5,37 @@ class Graph:
 
   def add_vertex(self, vertex):
     self.graph_dict[vertex.value] = vertex
-  
+
   def add_edge(self, from_vertex, to_vertex, weight = 0):
-    print("Adding edge from {0} to {1}".format(from_vertex.value, to_vertex.value))
-    self.weight = weight
     self.graph_dict[from_vertex.value].add_edge(to_vertex.value, weight)
     if not self.directed:
       self.graph_dict[to_vertex.value].add_edge(from_vertex.value, weight)
 
   def find_path(self, start_vertex, end_vertex):
-    print("Searchin from {0} to {1}".format(start_vertex, end_vertex))
     start = [start_vertex]
-    while start:
+    # Checkpoint 1, replace these comments:
+    # Use a dictionary to track which
+    # vertices we've already visited
+    seen = {}
+    while len(start) > 0:
       current_vertex = start.pop(0)
-      print("Current Vertex: " + current_vertex)
+      # Checkpoint 2, replace these comments:
+      # Update the `seen` variable
+      # now that we've visited current_vertex
+      seen[current_vertex] = True
+      print("Visiting " + current_vertex)
       if current_vertex == end_vertex:
         return True
-      vertex = self.graph_dict[current_vertex]
-      next_vertices = vertex.get_edges()
-      start += next_vertices
+      else:
+        vertex = self.graph_dict[current_vertex]
+        next_vertices = vertex.get_edges()
+        
+        # Filter next_vertices so it only
+        # includes vertices NOT IN seen
+        
+        # Checkpoint 3, uncomment and replace the question marks:
+        next_vertices = [vertex for vertex in next_vertices if vertex not in seen]
+        start.extend(next_vertices)
+        
     return False
 
-    
