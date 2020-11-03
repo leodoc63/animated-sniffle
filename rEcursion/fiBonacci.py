39721209563e128783bdb2bdc18068b49c99a2bb
oldoc <oldoc@localhost.localdomain>
2020-11-02 20:53:12 -0400

    finished

---
+++ rEcursion/fiBonacci.py
@@ -0,0 +1,12 @@
+def fibonacci(n):
+    #base cases
+    if n == 1:
+        return n
+    if n == 0:
+        return n
+    
+    #recursive step
+    print("Recursive call with {0} as input".format(n))
+    return fibonacci(n-1) + fibonacci(n-2)
+
+fibonacci(5)
