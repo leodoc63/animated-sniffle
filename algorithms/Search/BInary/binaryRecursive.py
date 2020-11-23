e638bac6b7116e009f780197ceb9dca42b81a60f
oldoc <oldoc@localhost.localdomain>
2020-11-23 19:21:23 -0400

    Base Case 2

--- algorithms/Search/BInary/binaryRecursive.py
+++ algorithms/Search/BInary/binaryRecursive.py
@@ -5,11 +5,11 @@ def binary_search(sorted_list, target):
     mid_idx = len(sorted_list) // 2
     mid_val = sorted_list[mid_idx]
     return mid_idx, mid_val
-
-
-
+    if mid_val == target:
+        return mid_idx
 
 # For testing:
 sorted_values = [13, 14, 15, 16, 17]
 print(binary_search([], 42))
 print(binary_search(sorted_values, 42))
+print(binary_search(sorted_values, 15))
